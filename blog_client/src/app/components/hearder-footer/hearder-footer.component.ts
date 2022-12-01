6c2ea718cf257004818b728b9bb3fa33cf5f33c1
TU <v.chuongtu1102@gmail.com>
2022-12-01 18:47:55 +0700

    header and footer

---
+++ blog_client/src/app/components/hearder-footer/hearder-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hearder-footer',
+  templateUrl: './hearder-footer.component.html',
+  styleUrls: ['./hearder-footer.component.scss']
+})
+export class HearderFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
